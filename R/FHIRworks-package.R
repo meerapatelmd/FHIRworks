0181eb886da9883a0ecc0d7b8b6dbf4ae867647f
Meera Patel <patelmeeray@gmail.com>
2020-12-16 18:57:05 -0500

    fix typo

--- R/FHIRworks-package.R
+++ R/FHIRworks-package.R
@@ -1,5 +1,5 @@
 #' @description
 #' FHIRworks is an R interface for making FHIR API calls.
-#' Learn more about FHIR at \url{http://build.fhir.org/http.html}.
+#' Learn more about FHIR at \url{http://build.fhir.org/}.
 #' @keywords internal
 "_PACKAGE"
